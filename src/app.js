956dfd10bc42f43382dac70dd8fd6c6a00f1c8f0
santiago-pp <santiagopereaap@gmail.com>
2025-10-20 19:43:51 +0000

    Primer Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let card = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10 ,'J', 'Q', 'K'];
+  let palo = ['♦', '♥', '♠', '♣'];
+  let color = ["black", "red"];
+
+  let randomCard = card[Math.floor(Math.random() * card.length)];
+  let randomPalo = palo[Math.floor(Math.random() * palo.length)];
+  let randomColor = color[Math.floor(Math.random() * color.length)];
+
+  /* console.log(randomCard)
+  console.log(randomPalo) */
+  console.log(randomColor)
+
+  let cardColor = document.querySelectorAll('cardColor');
+  for (let i = 0; i < cardColor.length; i++){
+    cardColor.style.color = randomColor;
+  }
+
+  document.querySelector('#cardNumber').innerHTML = randomCard;
+  document.querySelector('#cardType').innerHTML = randomPalo;
+  document.querySelector('#cardTypeRotate').innerHTML = randomPalo;
+
+  /* boton.addEventListener('click', function() {
+    resetearIntervalo()
+    resetearCard()
+  }) */
+
 };
