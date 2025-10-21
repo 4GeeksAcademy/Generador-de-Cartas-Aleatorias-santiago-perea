85e3f292a57d44b4049265b9fda8bd2576729991
santiago-pp <santiagopereaap@gmail.com>
2025-10-21 11:22:29 +0000

    Puntos Extra +1

--- src/app.js
+++ src/app.js
@@ -5,32 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  let card = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10 ,'J', 'Q', 'K'];
-  let palo = ['♦', '♥', '♠', '♣'];
+function randomCard() { 
+  let valor = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10 ,'J', 'Q', 'K'];
+  let iconos = ['♦', '♥', '♠', '♣'];
   let color = ["black", "red"];
-
-  let randomCard = card[Math.floor(Math.random() * card.length)];
-  let randomPalo = palo[Math.floor(Math.random() * palo.length)];
+  let randomValor = valor[Math.floor(Math.random() * valor.length)];
+  let randomIconos = iconos[Math.floor(Math.random() * iconos.length)];
   let randomColor = color[Math.floor(Math.random() * color.length)];
 
-  /* console.log(randomCard)
-  console.log(randomPalo) */
+  console.log(randomValor)
+  console.log(randomIconos)
   console.log(randomColor)
 
-  let cardColor = document.querySelectorAll('cardColor');
-  for (let i = 0; i < cardColor.length; i++){
-    cardColor.style.color = randomColor;
-  }
+  let cardColor = document.querySelector('#top');
+  let cardColor1 = document.querySelector('#bottom');
+  cardColor.style.color = randomColor; 
+  cardColor1.style.color = randomColor; 
+ 
+  document.querySelector('#cardNumber').textContent = randomValor;
+  document.querySelector('#top').textContent = randomIconos;
+  document.querySelector('#bottom').textContent = randomIconos;
+};
 
-  document.querySelector('#cardNumber').innerHTML = randomCard;
-  document.querySelector('#cardType').innerHTML = randomPalo;
-  document.querySelector('#cardTypeRotate').innerHTML = randomPalo;
-
-  /* boton.addEventListener('click', function() {
-    resetearIntervalo()
-    resetearCard()
-  }) */
-
+window.onload = function() {
+  //write your code here
+  randomCard(); 
+  let boton = document.querySelector('#boton');
+  boton.addEventListener("click", () => randomCard()); 
 };
